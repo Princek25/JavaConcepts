111eed0228d03b72d847b3a068c0f66ad258807d
Prince <princek2530@gmail.com>
2022-10-10 19:47:43 +0530

    Added: Array1

--- src/com/bridgelabz/arrays/Array1.java
+++ src/com/bridgelabz/arrays/Array1.java
@@ -1,6 +1,24 @@
 package com.bridgelabz.arrays;
 
-// Array
+// Advantages of Array:
+// 1. Array is a collection of similar types of data types
+// 2. When we need to store multiple values of similar data type in a single variable we use array
+// 3. Arrays are faster than primitive data types because it takes less memory
+// 4. The memory of arrays are store in heap area
+
+// Disadvantage of Array:
+// 1. the size of the array is fixed
+// 2. wastage of memory
+// 3. we can store only similar data type
 
 public class Array1 {
+
+//    instead of doing that we use array
+//    int empId1 = 101;
+//    int empId2 = 102;
+//    int empId3 = 103;
+//    int empId1000 = 103000;
+
+    // we can store all the employee id in a single variable
+    int empId [] = new int[1000];
 }
